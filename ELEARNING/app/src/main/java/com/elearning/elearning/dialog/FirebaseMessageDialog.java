affa78c9097f55084803660e164c77405d40bbb7
VoMinhQuan <quan.vo@covisoft.asia>
2017-07-06 22:09:17 +0700

    update dialog notification

--- ELEARNING/app/src/main/java/com/elearning/elearning/dialog/FirebaseMessageDialog.java
+++ ELEARNING/app/src/main/java/com/elearning/elearning/dialog/FirebaseMessageDialog.java
@@ -2,11 +2,16 @@ package com.elearning.elearning.dialog;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.support.v7.app.AlertDialog;
@@ -15,14 +20,14 @@ import com.elearning.elearning.R;
 import com.elearning.elearning.helper.ImageConvert;
 import com.elearning.elearning.network.APIConstant;
 import com.elearning.elearning.prefs.Constant;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 
 public class FirebaseMessageDialog extends AppCompatActivity {
-    private AlertDialog.Builder builder;
-    private AlertDialog dialog;
     private TextView txtTitle, txtBody;
     private ImageView imgContent;
+    private Button btnCancel, btnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +35,21 @@ public class FirebaseMessageDialog extends AppCompatActivity {
         String title = getIntent().getExtras().getString(Constant.FIREBASE_TITLE);
         String message = getIntent().getExtras().getString(Constant.FIREBASE_MSG);
         String urlImage = getIntent().getExtras().getString(Constant.FIREBASE_IMAGE);
-        builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = this.getLayoutInflater();
-        View view = inflater.inflate(R.layout.dialog_notification, null);
-        builder.setView(view);
-        dialog = builder.create();
-        dialog.setCancelable(false);
-        dialog.setCanceledOnTouchOutside(false);
-        txtTitle = (TextView) view.findViewById(R.id.txtTitle);
-        txtBody = (TextView) view.findViewById(R.id.txtBody);
-        imgContent = (ImageView) view.findViewById(R.id.imageNotification);
-        try {
-            //set value
-            txtTitle.setText(title);
-            txtBody.setText(message);
-            imgContent.setBackground(ImageConvert.getDrawableFromUrl(APIConstant.HOST_NAME_IMAGE + urlImage));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        view.findViewById(R.id.imgCancel).setOnClickListener(new View.OnClickListener() {
+        final View notificationView = LayoutInflater
+                .from(this).inflate(R.layout.dialog_notification, (ViewGroup) findViewById(R.id.dialog_notification));
+        txtTitle = (TextView) notificationView.findViewById(R.id.txtTitle);
+        txtBody = (TextView) notificationView.findViewById(R.id.txtBody);
+        imgContent = (ImageView) notificationView.findViewById(R.id.imageNotification);
+        btnCancel = (Button) notificationView.findViewById(R.id.btnCancel);
+        btnView = (Button) notificationView.findViewById(R.id.btnView);
+        //set value
+        txtTitle.setText(title);
+        txtBody.setText(message);
+        Picasso.with(this)
+                .load(APIConstant.HOST_NAME_IMAGE + urlImage)
+                .resize(200, 200)
+                .into(imgContent);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NotificationManager notificationManager =
@@ -59,6 +60,9 @@ public class FirebaseMessageDialog extends AppCompatActivity {
                 finish();
             }
         });
-        dialog.show();
+        final AlertDialog shoppingDialog = new AlertDialog.Builder(this)
+                .setView(notificationView)
+                .create();
+        shoppingDialog.show();
     }
 }
